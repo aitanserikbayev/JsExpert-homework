3a0ef0a47ca447cb2e720fb944091f32eb550d7e
Aitan <aitan.serikbayev@gmail.com>
2020-09-10 12:01:32 +0600

    Course Javascript[Middle]. Session 4 part 1 Homework - Code is optimized using functions

--- c1-s8/js/main.js
+++ c1-s8/js/main.js
@@ -1,25 +1,52 @@
 let total = 0;
-const result = document.getElementById("result");
+let result = "";
+const resultContainer = document.getElementById("result");
 
-for (let i = 0; i < 15; i++) {
-  let first = Math.floor((Math.random() * 6) + 1);
-  let second = Math.floor((Math.random() * 6) + 1);
+(function run() {
+  for (let i = 0; i < 15; i++) {
+    let first = getRndNumber();
+    let second = getRndNumber();
 
-  if (i == 8 || i == 13) {
-    continue;
+    if (i == 8 || i == 13) {
+      continue;
+    }
+
+    setResult("Первая кость: " + first + " Вторая кость " + second + "<br/>");
+
+    isNumbersEqual(first, second);
+
+    isBigDifference(first, second);
+
+    setTotal(first, second);
   }
 
-  result.innerHTML += "Первая кость: " + first + " Вторая кость " + second + "<br/>";
+  printResult();
+}());
 
-  if (first == second) {
-    result.innerHTML += "<i>Выпал дубль. Число " + first + "</i><br/>";
-  }
-
-  if (first < 3 && second > 4 || first > 4 && second < 3) {
-    result.innerHTML += "<i>Большой разброс между костями. Разница составляет " + Math.abs((second - first)) + "</i><br/>";
-  }
-
-  total += first + second;
+function getRndNumber() {
+  return Math.floor((Math.random() * 6) + 1);
 }
 
-total > 100 ? result.innerHTML += "<strong>Победа, вы набрали " + total + " очков</strong>" : result.innerHTML += "<strong>Вы проиграли, у вас " + total + " очков<strong/>"
+function setResult(str) {
+  return result += str;
+}
+
+function isNumbersEqual(a, b) {
+  if (a == b) {
+    setResult("<i>Выпал дубль. Число " + a + "</i><br/>");
+  }
+}
+
+function isBigDifference(a, b) {
+  if (a < 3 && b > 4 || a > 4 && b < 3) {
+    setResult("<i>Большой разброс между костями. Разница составляет " + Math.abs((b - a)) + "</i><br/>");
+  }
+}
+
+function setTotal(a, b) {
+  total += a + b;
+}
+
+function printResult() {
+  resultContainer.innerHTML = setResult(total > 100 ? "<strong>Победа, вы набрали " + total + " очков</strong>" : "<strong>Вы проиграли, у вас " + total + " очков<strong/>");
+}
